cd5efbf7208c17c2fb4d4e60416d598e46edcb58
Jesús López Sánchez <lopsanjesus@gmail.com>
2021-02-17 23:43:53 +0100

    First view

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from "./spotify.ico";
-import "./App.css";
+import React, { useRef, useEffect } from 'react';
+import { useLocation, Switch } from 'react-router-dom';
+import AppRoute from './utils/AppRoute';
+import ScrollReveal from './utils/ScrollReveal';
+import ReactGA from 'react-ga';
 
-function App() {
+// Layouts
+import LayoutDefault from './layouts/LayoutDefault';
+
+// Views 
+import Home from './views/Home';
+
+// Initialize Google Analytics
+ReactGA.initialize(process.env.REACT_APP_GA_CODE);
+
+const trackPage = page => {
+  ReactGA.set({ page });
+  ReactGA.pageview(page);
+};
+
+const App = () => {
+
+  const childRef = useRef();
+  let location = useLocation();
+
+  useEffect(() => {
+    const page = location.pathname;
+    document.body.classList.add('is-loaded')
+    childRef.current.init();
+    trackPage(page);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h2>The song of the day</h2>
-      </header>
-      <section>
-        <h3>18 / febrero / 2021</h3>
-        <iframe
-          className="Song-youtube-iframe"
-          src="https://www.youtube.com/embed/wuJIqmha2Hk"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-
-        <img src={logo} className="Spotify-icon" alt="listen on spotiyf" />
-        <a href="spotify:track:2RlgNHKcydI9sayD2Df2xp">Escuchala en Spotify</a>
-      </section>
-    </div>
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
+        </Switch>
+      )} />
   );
 }
 
---
+++ src/layouts/LayoutDefault.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+
+const LayoutDefault = ({ children }) => (
+  <>
+    <main className="site-content">{children}</main>
+    <Footer />
+  </>
+);
+
+export default LayoutDefault;
